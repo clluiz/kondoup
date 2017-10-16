6af9f306221afb2639835fc6241f3b0a3adcd2aa
Cleiton Luiz <cluiz@mcontigo.com>
2017-10-16 21:48:56 -0200

    Criação da validação dos formulários ao clicar em salvar

--- src/app/cadastro.usuario.component.ts
+++ src/app/cadastro.usuario.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
     // tslint:disable-next-line:component-selector
@@ -7,14 +8,29 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
     templateUrl: './cadastro.usuario.component.html',
 })
 export class CadastroUsuarioComponent {
-  title = 'KondoUP';
+    title = 'KondoUP';
 
-  formUsuario = new FormGroup({
+    constructor(private router: Router) { }
 
-    name: new FormControl('', Validators.required),
-    username: new FormControl('', Validators.required),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    phone: new FormControl(),
-    website: new FormControl()
-  });
+    formUsuario = new FormGroup({
+        name: new FormControl('', Validators.required),
+        username: new FormControl('', Validators.required),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        phone: new FormControl(),
+        website: new FormControl()
+    });
+    cancelar() {
+        this.router.navigateByUrl('/usuarios');
+    }
+    cadastrar() {
+        Object.keys(this.formUsuario.controls).forEach(field => {
+            const control = this.formUsuario.get(field);
+            control.markAsDirty({ onlySelf: true });
+        });
+        if (this.formUsuario.valid) {
+
+        } else {
+
+        }
+    }
 }
